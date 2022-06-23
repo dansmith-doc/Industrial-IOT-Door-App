9879ada43d8e5a5aba3e9ab6031e3323d8796c77
Dan Smith <danielsmith198485@hotmail.com>
2022-06-23 19:01:28 +0100

    Modify setup scripts

--- Scripts/setup-docker.sh
+++ Scripts/setup-docker.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-echo "Updating System..."
-sudo apt update && sudo apt upgrade -y
-echo "Done"
-sleep 10
+echo "Installing dependencies if needed...."
+sleep 5
+sudo apt install ca-certificates curl gnupg lsb-release -y
+sleep 5
 
+echo "Getting Docker setup...."
+curl -fsSL https://get.docker.com -o get-docker.sh
+sleep 5
+
+echo "Installing...."
+sleep 5
+./get-docker.sh
+sleep 5
+
+echo "Creating Docker group user...."
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sleep 5
+
+echo "Creating Service....."
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+echo "Done...."
+
+sleep 5
+echo "LOGOUT AND LOGIN TO ENABLE PERMISSIONS"
